74810046c28183f334ffed3bb3e09dd0812426eb
The Spiteful Octopus <Mike@MSec.ca>
2016-04-01 19:16:59 -0600

    adding more routes and learning about dynamic routes and status codes

--- app.py
+++ app.py
@@ -11,6 +11,39 @@ app = Flask(__name__)
 def hello_world():
 	return "Hello, World!"
 
+
+# adding a dynamic route
+@app.route('/test/<search_query>')
+def search(search_query):
+	return "Hello " + search_query
+
+
+#dynamic with int
+@app.route('/integer/<int:value>')
+def int_type(value):
+	print value + 1
+	return "correct"
+
+#dynamic with float
+@app.route('/float/<float:value>')
+def float_type(value):
+	print value + 1
+	return "correct"
+
+#dynamic route that accepts slashes
+@app.route('/path/<path:value>')
+def path_type(value):
+	print value
+	return  "correct"
+
+
+@app.route('/name/<name>')
+def index(name):
+	if name.lower() == "michael":
+		return "Hello {}".format(name), 200
+	else:
+		return "Not Found", 404
+
 # start the development server using the run method
 if __name__ == "__main__":
 	app.run(debug=True)
